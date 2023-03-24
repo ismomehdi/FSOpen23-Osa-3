dcfcb216f8354fcf43192ce906e42be066a1158b
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-03-24 20:51:16 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -14,7 +14,11 @@ mongoose.connect(url)
   })
 
 const peopleSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
     number: String,
   })
 
